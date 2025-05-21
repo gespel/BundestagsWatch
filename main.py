989b9451f08ca92aebeed9f5c02498d94c51eb33
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2025-05-21 15:17:57 +0200

    fix from the fix

--- main.py
+++ main.py
@@ -94,10 +94,11 @@ class BundestagsWatch:
     def render_plot(self):
         party_ids = ["5", "1", "2", "3", "4", "7", "23"]
         dataframes = [self.plot_party(pid, 1) for pid in party_ids]
-        dataframes = [df.reset_index(drop=True) for df in dataframes]
+        #dataframes = [df.reset_index(drop=True) for df in dataframes]
+        dataframes = [df[~df.index.duplicated()] for df in dataframes]
         all_data = pd.concat(dataframes, axis=1, join='outer')
         smoothed_data = all_data.rolling(window=20, min_periods=1).mean()
-        #print(smoothed_data)
+        print(smoothed_data)
         plt.figure(figsize=(12, 6))
         for party_id in party_ids:
             plt.plot(smoothed_data.index, smoothed_data[party_id], antialiased=True, color=self.get_color_for_party(party_id), linewidth=2, label=f'{self.party_name_by_id(party_id)}')
